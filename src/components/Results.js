5c0dbce4d0edc607c9d4e535f1d3bfa35cb9a69f
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2021-06-16 15:31:38 +0530

    Handle error and loading states

--- src/components/Results.js
+++ src/components/Results.js
@@ -5,6 +5,7 @@ import imdb from "../assets/imdb-logo.png";
 const Results = () => {
   const [result, setResult] = useState({});
   const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
   const {
     location: { search = "?q=" },
   } = useHistory();
@@ -14,49 +15,69 @@ const Results = () => {
       .then((res) => res.json())
       .then((data) => {
         setResult(data);
+        setLoading(false);
       })
-      .catch((err) => console.log(err))
-      .finally(() => setLoading(false));
+      .catch((err) => {
+        console.log(err);
+        setError(true);
+      });
   }, [search]);
 
-  return !loading ? (
-    <main>
-      <div className="search-results">
-        <div className="card">
-          <img className="poster" src={result.poster} alt={result.title} />
-          <div className="links">
-            {result.ottProviders.map((ott) => (
-              <a
-                target="_blank"
-                href={ott.url}
-                rel="noreferrer"
-                key={ott.provider}
-              >
-                <img src={ott.icon} alt={ott.provider} />
-              </a>
-            ))}
+  return (
+    <>
+      {error && <p> 💀 No Results found</p>}
+      {!error && (
+        <main>
+          {loading ? (
+            <div className="loader"></div>
+          ) : (
+            <>
+              <div className="search-results">
+                <div className="card">
+                  <img
+                    className="poster"
+                    src={result.poster}
+                    alt={result.title}
+                  />
+                  <div className="links">
+                    {result.ottProviders.map((ott) => (
+                      <a
+                        target="_blank"
+                        href={ott.url}
+                        rel="noreferrer"
+                        key={ott.provider}
+                      >
+                        <img src={ott.icon} alt={ott.provider} />
+                      </a>
+                    ))}
+                  </div>
+                  <div className="imdb">
+                    <img width="24px" src={imdb} alt="imdbRating" />
+                    <span>{result.imdb}</span>
+                  </div>
+                </div>
+              </div>
+            </>
+          )}
+          <div className="search-suggestions-links">
+            {!loading && (
+              <div className="search-suggestions">
+                <h3>Similar Results</h3>
+                {result.searchSuggestions.map((item, index) => (
+                  <a
+                    key={item + index}
+                    href={`/results?q=${item}&locale=en_IN&type=`}
+                  >
+                    {item}
+                  </a>
+                ))}
+              </div>
+            )}
           </div>
-          <div className="imdb">
-            <img width="24px" src={imdb} alt="imdbRating" />
-            <span>{result.imdb}</span>
-          </div>
-        </div>
-      </div>
-      <div className="search-suggestions">
-        <div className="search-suggestions-links">
-          <h3>Similar Results</h3>
-          {result.searchSuggestions.map((item, index) => (
-            <a
-              key={item + index}
-              href={`/results?q=${item}&locale=en_IN&type=`}
-            >
-              {item}
-            </a>
-          ))}
-        </div>
-      </div>
-    </main>
-  ) : null;
+        </main>
+      )}
+    </>
+  );
 };
 
 export default Results;
