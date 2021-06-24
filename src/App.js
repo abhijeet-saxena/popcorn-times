8d25f11cb61ed722779b186ce6eba11cee145fcc
Abhijeet Saxena <saxenaabhijeet@outlook.com>
2021-06-25 00:00:00 +0530

    Add basc routes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,21 @@
 import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Home";
+import Results from "./Results";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Router>
+        <Switch>
+          <Route path="/results" exact>
+            <Results />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/Results.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Results = () => {
+  return <div>Results</div>;
+};
+
+export default Results;
